642b89ab7999232d49c1aeb44246fdb15456945c
Andrey <kochetpwr@gmail.com>
2021-10-11 12:55:28 +0400

    first commit

---
+++ functions.py
@@ -0,0 +1,184 @@
+from math import *
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+plusMinus = ['+',"-"]
+
+def f(x, fn):
+    fn= fn.replace(' ' ,'')
+    return float(recFunction(x, fn))  
+
+def findBrackets(func):
+    start = func.find("(")
+    i = start + 1
+    n = 1
+    while n != 0:
+        if func[i] == "(":
+            n += 1
+        elif func[i] == ")":
+            n -= 1
+        i += 1 
+    end = i
+    return start, end
+
+def findRight(fn, pos):
+    start = pos + 1
+    n = start
+    while fn[n] in numbers and n < len(fn) - 1 or n == start and fn[n] == "-":
+        n += 1
+    if n == len(fn) - 1:
+        end = n + 1
+    else:
+        end = n
+    return start, end
+
+def findLeft(fn, pos):
+    end = pos
+    n = end - 1 
+    while fn[n] in numbers and n >= 0:
+        n -= 1
+    if n != -1 and fn[n] in plusMinus:
+        start = n
+    else:
+        start = n + 1
+    return start, end
+
+def findLeftRight(func, pos):
+    startRight, endRight = findRight(func, pos)
+    startLeft, endLeft = findLeft(func, pos)
+    return startLeft, endLeft, startRight, endRight
+
+def updateStr(fn, start, end, n):
+    if n < 0:
+        if start != 0 and fn[start - 1] == "+":
+            fn = fn[: start - 1] + strDeleteE(n) + fn[end :]
+        elif start != 0 and fn[start - 1] == "-":
+            fn = fn[: start - 1] +"+"+ strDeleteE(abs(n)) + fn[end :]
+        else:
+            fn = fn[: start] + strDeleteE(n) + fn[end :]
+    else:
+        fn = fn[: start] + strDeleteE(n) + fn[end :] 
+    return fn  
+
+def updateStr2(fn, start, end, n):
+    if n > 0 and (fn[start] == "+" or fn[start] == "-"):
+        fn = fn[: start] +"+"+ strDeleteE(n) + fn[end :]
+    else:
+        fn = fn[: start] + strDeleteE(n) + fn[end :]      
+    return fn
+
+def strDeleteE(x):
+    x = str(x)
+    if x.find("e") != -1:        
+        nE = x.find("e")
+        if x[nE + 1] == "+":
+            st = int(x[nE + 2 :]) - 12
+            nDot = x.find(".")
+            x = x[: nDot] + x[nDot + 1 : nE]
+            for i in range(0, st):
+                x = x + "0"
+    return x
+
+def recFunction(x, fn):
+    while fn.find("(") != -1:
+        start, end = findBrackets(fn)
+        insideBracets = fn[start + 1 : end - 1]
+        fn = fn[: start] + recFunction(x, insideBracets) + fn[end :]
+
+    while fn.find("x^") != -1:
+        start, end = findRight(fn, fn.find("x^") + 1)
+        n = float(fn[start : end])
+        pw = pow(x, n)
+        fn = updateStr(fn, start - 2, end, pw)   
+    
+    while fn.find("x*") != -1:
+        start, end = findRight(fn, fn.find("x*") + 1)
+        n = float(fn[start : end])
+        s = x * n 
+        fn = updateStr(fn, start - 2, end, s)
+
+    while fn.find("*x") != -1:
+        start, end = findLeft(fn, fn.find("*x"))
+        n = float(fn[start : end])
+        s = x * n 
+        fn = updateStr2(fn, start, end + 2, s)  
+
+    while fn.find("x") != -1:
+        start  = fn.find("x")
+        fn = updateStr(fn, start, start + 1, x)
+
+    while fn.find("e^") != -1:
+        start, end = findRight(fn, fn.find("e^") + 1)
+        n = float(fn[start : end])
+        pw = pow(exp(1), n)
+        fn = updateStr(fn, start - 2, end, pw)     
+
+    while fn.find("*") != -1 or fn.find("/") != -1:
+        if fn.find("*") != -1 and fn.find("/") != -1:
+            if fn.find("*") < fn.find("/"):
+                startLeft, endLeft, startRight, endRight  = findLeftRight(fn, fn.find("*"))
+                a = float(fn[startLeft : endLeft])
+                b = float(fn[startRight : endRight])        
+                s = a * b
+            else:
+                startLeft, endLeft, startRight, endRight  = findLeftRight(fn, fn.find("/"))
+                a = float(fn[startLeft : endLeft])
+                b = float(fn[startRight : endRight])        
+                s = a / b
+        elif fn.find("*") != -1:
+            startLeft, endLeft, startRight, endRight  = findLeftRight(fn, fn.find("*"))
+            a = float(fn[startLeft : endLeft])
+            b = float(fn[startRight : endRight])        
+            s = a * b
+        else:
+            startLeft, endLeft, startRight, endRight  = findLeftRight(fn, fn.find("/"))
+            a = float(fn[startLeft : endLeft])
+            b = float(fn[startRight : endRight])        
+            s = a / b
+        fn = updateStr2(fn, startLeft, endRight, s)
+
+    while fn.find("+") != -1 or fn.find("-") != -1 and fn.rfind("-") != 0: 
+        if fn.find("+") != -1 and fn.find("-") != -1 and fn.rfind("-") != 0:
+            if fn.find("+") <  fn[1:].find("-"):
+                startLeft, endLeft, startRight, endRight,  = findLeftRight(fn, fn.find("+"))
+                a = float(fn[startLeft : endLeft])
+                b = float(fn[startRight : endRight])        
+                s = a + b                
+            else:
+                if fn.find("-") == 0:
+                    startLeft, endLeft, startRight, endRight,  = findLeftRight(fn, fn[1 :].find("-"))
+                else:
+                    startLeft, endLeft, startRight, endRight,  = findLeftRight(fn, fn.find("-"))        
+                a = float(fn[startLeft : endLeft])
+                b = float(fn[startRight : endRight])        
+                s = a - b
+        elif fn.find("+") != -1:
+            startLeft, endLeft, startRight, endRight,  = findLeftRight(fn, fn.find("+"))
+            a = float(fn[startLeft : endLeft])
+            b = float(fn[startRight : endRight])        
+            s = a + b
+        else:
+            if fn.find("-") == 0:
+                    startLeft, endLeft, startRight, endRight,  = findLeftRight(fn, fn[1 :].find("-"))
+            else:
+                startLeft, endLeft, startRight, endRight,  = findLeftRight(fn, fn.find("-"))        
+            a = float(fn[startLeft : endLeft])
+            b = float(fn[startRight : endRight])        
+            s = a - b
+
+        fn = fn[: startLeft] + strDeleteE(s) + fn[endRight :]
+    
+    return fn
+
+def method_chord(func,xPrev, xCurr, e):
+    Xs = []
+    Xs.append((1, xPrev, 0))
+    Xs.append((2, xCurr, xCurr - xPrev))
+    xNext = xCurr - f(xCurr, func) * (xCurr - xPrev) / (f(xCurr, func) - f(xPrev, func))
+    Xs.append((3, xNext, xNext - xCurr))
+    i = 4
+    while abs(xNext - xCurr) > e:
+        xPrev = xCurr
+        xCurr = xNext
+        xNext = xCurr - f(xCurr, func) * (xCurr - xPrev) / (f(xCurr, func) - f(xPrev, func))
+        Xs.append((i, xNext, xNext - xCurr))
+        i += 1
+    return xNext, Xs
---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, session, render_template, redirect, url_for
+
+from functions import method_chord
+import os
+
+result = 0
+Xs = []
+def main_page(): 
+    global result
+    global Xs
+    if request.method =="GET":
+        return render_template('html\main_page.html')
+    else:
+        fn = request.form.get('function')
+        x0 = float(request.form.get('x0'))
+        x1 = float(request.form.get('x1'))
+        e = float(request.form.get('e'))
+        result, Xs = method_chord(fn, x0, x1, e)
+        return redirect(url_for('result'))
+
+def second_page():
+    return render_template('html\result_page.html', res=result, Xs=Xs )
+
+
+    
+
+folder = os.getcwd()
+app = Flask(__name__, template_folder=folder, static_folder=folder) 
+
+app.add_url_rule('/', 'main_page', main_page, methods=['post', 'get'])
+app.add_url_rule('/result', 'result', second_page) # создаёт правило для URL '/test'
+
+if __name__ == "__main__":
+    app.run()
